db72c3857429d27b5ff35057dba77984cb4d83e7
Yana Kamenka <32295854+pedorgia@users.noreply.github.com>
2026-03-01 21:06:52 +0300

    д/3 6

---
+++ Task12.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// 1
+public record Employee1(string Name, int Id, string Department);
+// 2
+public record Employee2(string Name, int Id, string Department, decimal Salary);
+
+class Task12
+{
+    static void Main()
+    {
+        // 1
+        var employee1 = new Employee1("Яна", 6, "IT");
+        var employee2 = new Employee1("Яна", 6, "IT");
+
+        bool equals = employee1.Equals(employee2);
+        bool operatorEquals = (employee1 == employee2);
+
+        Console.WriteLine($"Результат .Equals - {equals}");
+        Console.WriteLine($"Результат == - {operatorEquals}");
+
+        // 2
+        var employee = new Employee2("Яна", 215, "IT", 1000);
+
+        decimal bonus = 500;
+        Employee2 promotedEmployee = Promote(employee, bonus);
+
+        Console.WriteLine("Cотрудник до повышения:");
+        Console.WriteLine(employee);
+
+        Console.WriteLine("Сотрудник после повышения:");
+        Console.WriteLine(promotedEmployee);
+    }
+    // 2
+    public static Employee2 Promote(Employee2 emp, decimal bonus)
+    {
+        return emp with { Salary = emp.Salary + bonus };
+    }
+}
+
---
+++ Task3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+class Program
+{
+    // 3
+    static async Task Main()
+    {
+        Task<string> taskA = GetDataAsync("Задержка 3000", 3000);
+        Task<string> taskB = GetDataAsync("Задержка 1000", 1000);
+
+        Task<string> completedTask = await Task.WhenAny(taskA, taskB);
+
+        string result = await completedTask;
+        Console.WriteLine($"Победитель: {result}");
+    }
+
+    public static async Task<string> GetDataAsync(string sourceName, int delayMs)
+    {
+        await Task.Delay(delayMs);
+        return $"Данные от {sourceName}";
+    }
+}
