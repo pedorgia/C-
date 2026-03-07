caed58ace156c94cc78bd2fc8c195481035eaa43
Yana Kamenka <32295854+pedorgia@users.noreply.github.com>
2026-03-07 20:43:41 +0300

    д/з урок 12

---
+++ Common.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace HomeWork12
+{
+    class Person(string name, int age, string email, bool isStudent)
+    {
+        public string Name { get; set; } = name;
+        public int Age { get; set; } = age;
+        public string Email { get; set; } = email;
+        public bool IsStudent { get; set; } = isStudent;
+    }
+
+    class ReflectionTest
+    {
+        public static void PrintProperties(object obj)
+        {
+            if (obj == null) return;
+            var type = obj.GetType();
+            Console.WriteLine($"Тип объекта: {type.Name}");
+
+            var properties = type.GetProperties();
+            foreach (PropertyInfo prop in properties)
+            {
+                var value = prop.GetValue(obj);
+                Console.WriteLine($"{prop.Name}: {value}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+using HomeWork12;
+
+class Program
+{
+    static void Main()
+    {
+        // 1
+        Person person = new("Яна", 27, "yanochka@gmail.com", true);
+        ReflectionTest.PrintProperties(person);
+
+        // 2
+        var inputPath = "input.txt";
+        var outputPath = "output.txt";
+        File.WriteAllLines(inputPath, ["Авокадо", "Манго", "Сыр", "Яйца"]);
+
+        var lineCount = 0;
+        var charCount = 0;
+        List<string> lines = [];
+        using (StreamReader reader = new(inputPath))
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                lineCount++;
+                charCount += line.Length;
+                lines.Add(line);
+            }
+        }
+
+        using StreamWriter writer = new(outputPath);
+        writer.WriteLine($"Количество строк: {lineCount}");
+        writer.WriteLine($"Количество символов: {charCount}");
+        writer.WriteLine("Содержимое: ");
+
+        foreach (var line in lines)
+        {
+            writer.WriteLine(line);
+        }
+    }
+}
